c00dbef02879e2298b46c9e202eeafb5d8633a05
vbentertaintments-hub <vbentertaintments@gmail.com>
2026-03-04 14:34:21 +0530

    fix typescript api types

--- pages/api/auth.ts
+++ pages/api/auth.ts
@@ -1,4 +1,6 @@
-export default function handler(req, res) {
+import type { NextApiRequest, NextApiResponse } from "next";
+
+export default function handler(req: NextApiRequest, res: NextApiResponse) {
   const { shop } = req.query;
 
   if (!shop) {

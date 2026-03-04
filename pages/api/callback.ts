d9bca1a914924a14f61ee7942b98bfc7b9b67fdb
vbentertaintments-hub <vbentertaintments@gmail.com>
2026-03-04 12:00:57 +0530

    fix typescript api types

--- pages/api/callback.ts
+++ pages/api/callback.ts
@@ -1,4 +1,6 @@
-export default async function handler(req, res) {
+import type { NextApiRequest, NextApiResponse } from "next";
+
+export default function handler(req: NextApiRequest, res: NextApiResponse) {
   res.json({
     success: true,
     message: "OAuth callback working"
